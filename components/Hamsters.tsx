ad57f04771330df29c9e337810e033d2bc7d210a
jwooook <pntitgld@gmail.com>
2026-04-30 18:22:37 +0900

    hammy update

--- components/Hamsters.tsx
+++ components/Hamsters.tsx
@@ -2,13 +2,14 @@ import Image from "next/image";
 
 export default function Hamsters({ className = "" }: { className?: string }) {
   return (
-    <div className="flex gap-12 ${className}'}">
-      <Image src="/chichi_arm.png" alt="hamster" width={220} height={220} />
-      <Image src="/chichi_arm.png" alt="hamster" width={220} height={220} />
-      <Image src="/chichi_arm.png" alt="hamster" width={220} height={220} />
-      <Image src="/chichi_arm.png" alt="hamster" width={220} height={220} />
-      <Image src="/chichi_arm.png" alt="hamster" width={220} height={220} />
-      <Image src="/chichi_arm.png" alt="hamster" width={220} height={220} />
+    <div className="flex gap-10 ${className}'}">
+      <Image src="/chichi_arm.PNG" alt="hamster" width={220} height={220} />
+      <Image src="/chichi_arm.PNG" alt="hamster" width={220} height={220} />
+      <Image src="/chichi_arm.PNG" alt="hamster" width={220} height={220} />
+      <Image src="/chichi_arm.PNG" alt="hamster" width={220} height={220} />
+      <Image src="/chichi_arm.PNG" alt="hamster" width={220} height={220} />
+      <Image src="/chichi_arm.PNG" alt="hamster" width={220} height={220} />
+      <Image src="/chichi_arm.PNG" alt="hamster" width={220} height={220} />
     </div>
   );
 }
