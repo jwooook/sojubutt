32ea1d73795a67b97f053cf0d3b5d57bb5acc89d
jwooook <pntitgld@gmail.com>
2026-04-30 17:36:56 +0900

    proootoooo

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,26 @@
 import Image from "next/image";
+import Manifesto from "@/components/Manifesto";
+import Letter from "@/components/Letter";
+import Hamsters from "@/components/Hamsters";
+import Logo from "@/components/Logo";
 
 export default function Home() {
   return (
-    <main className="flex items-center justify-center min-h-screen bg-[#c00500]">
-      <Image
-        src="/maaiii.PNG"
-        alt="sojubutt logo"
-        width={400}
-        height={400}
-      />
+    <main className="relative min-h-screen bg-[#e10600]">
+
+      {/* Centered logo */}
+      <div className="absolute inset-0 flex items-center justify-center">
+        <Logo />
+      </div>
+
+      <div className="absolute top-4 left-4 right-4">
+        <Manifesto />
+      </div>
+      
+      <div className="absolute top-1/2 left-4 right-4 -translate-y-1/2 mt-12">
+        <Letter />
+      </div>
+
     </main>
   );
 }
