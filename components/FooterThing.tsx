b1ed85cd9d03f67a618cb1cfd6fc8a430b104d98
jwooook <30074289+jwooook@users.noreply.github.com>
2026-05-08 18:21:43 +0900

    tweakagain

--- components/FooterThing.tsx
+++ components/FooterThing.tsx
@@ -51,7 +51,7 @@ export default function FooterThing({
     >
       <div className="space-y-2">
         <p className="font-space lowercase text-[#FFFFFF] font-semibold">
-          btw nothing here is useful. but maybe i'll send something useful back.
+          btw nothing here is useful. but maybe i'll send something useful.
         </p>
       </div>
 
